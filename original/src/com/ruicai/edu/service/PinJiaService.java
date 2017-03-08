cb7dec98ad50a9e315fda44560e6d8bb10870829
lws <chenkun_lws@126.com>
2017-03-08 05:54:44 +0000

    删除多余文件

--- original/src/com/ruicai/edu/service/PinJiaService.java
+++
@@ -1,9 +0,0 @@
-package com.ruicai.edu.service;
-
-import java.util.List;
-
-import com.ruicai.edu.entity.Relation;
-
-public interface PinJiaService {
-  public abstract List<Relation> select(int s_id);
-}
