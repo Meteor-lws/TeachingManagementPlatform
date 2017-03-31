ddd214354a1cad805de2c127fce2833800942070
lws <chenkun_lws@126.com>
2017-03-31 01:18:38 +0000

    使用dependenciesManagement管理项目依赖

--- projects/TeachingManagement/src/main/java/com/ruicai/teaching/controller/TeachingController.java
+++ projects/TeachingManagement/src/main/java/com/ruicai/teaching/controller/TeachingController.java
@@ -9,18 +9,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
  *
  * Created by lws on 2017/3/30.
  */
+@SuppressWarnings("SameReturnValue")
 @Controller
 public class TeachingController {
 
     @RequestMapping(value = "/front", method = RequestMethod.GET)
     public String front() {
-        System.out.println("hh");
         return "front";
     }
 
     @RequestMapping(value = "/background", method = RequestMethod.GET)
     public String background() {
-        System.out.println("gg");
         return "background";
     }
 }
