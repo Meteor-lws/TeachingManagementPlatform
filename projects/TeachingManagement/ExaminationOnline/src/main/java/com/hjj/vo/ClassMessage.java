9153eb35b17755c02b40fb398e6004844009f015
zg <651413411@qq.com>
2017-04-01 07:25:26 +0000

    朱高第一次上传（添加在线考试项目结构）

---
+++ projects/TeachingManagement/ExaminationOnline/src/main/java/com/hjj/vo/ClassMessage.java
@@ -0,0 +1,95 @@
+package com.hjj.vo;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+/**
+ * Created by 朱高 on 2017/3/21.
+ */
+public class ClassMessage {
+    private String id;
+    @JSONField(serialize = false)
+    private String ids;
+    private String className;
+    private String testPhase;
+    private String testTime;
+    private String invigilator;
+    private String testPlace;
+    private String stuName;
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public String getIds() {
+        return ids;
+    }
+
+    public void setIds(String ids) {
+        this.ids = ids;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getTestPhase() {
+        return testPhase;
+    }
+
+    public void setTestPhase(String testPhase) {
+        this.testPhase = testPhase;
+    }
+
+    public String getTestTime() {
+        return testTime;
+    }
+
+    public void setTestTime(String testTime) {
+        this.testTime = testTime;
+    }
+
+    public String getInvigilator() {
+        return invigilator;
+    }
+
+    public void setInvigilator(String invigilator) {
+        this.invigilator = invigilator;
+    }
+
+    public String getTestPlace() {
+        return testPlace;
+    }
+
+    public void setTestPlace(String testPlace) {
+        this.testPlace = testPlace;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassMessage{" +
+                "id='" + id + '\'' +
+                ", ids='" + ids + '\'' +
+                ", className='" + className + '\'' +
+                ", testPhase='" + testPhase + '\'' +
+                ", testTime='" + testTime + '\'' +
+                ", invigilator='" + invigilator + '\'' +
+                ", testPlace='" + testPlace + '\'' +
+                '}';
+    }
+}
---
+++ projects/TeachingManagement/ExaminationOnline/src/main/java/com/hjj/vo/Echarts.java
@@ -0,0 +1,71 @@
+package com.hjj.vo;
+
+import java.util.List;
+
+/**
+ * Created by 朱高 on 2017/3/23.
+ */
+public class Echarts {
+    private List categories;
+    private List<List> data;
+    private List legend;
+    private String name;
+    private int value;
+
+    public Echarts(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public Echarts() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    /*private Map<String, List<Integer>> map;
+
+    public Map<String, List<Integer>> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, List<Integer>> map) {
+        this.map = map;
+    }*/
+
+    public List getLegend() {
+        return legend;
+    }
+
+    public void setLegend(List legend) {
+        this.legend = legend;
+    }
+
+    public List getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List categories) {
+        this.categories = categories;
+    }
+
+    public List<List> getData() {
+        return data;
+    }
+
+    public void setData(List<List> data) {
+        this.data = data;
+    }
+}
