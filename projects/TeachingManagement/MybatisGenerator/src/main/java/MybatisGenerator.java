4e45a66a12dc0fa41ebc1ddb9dce7ce98cfbb17e
lws <chenkun_lws@126.com>
2017-04-08 09:37:05 +0000

    根据逆向配置文件进行数据库逆向

--- projects/TeachingManagement/MybatisGenerator/src/main/java/MybatisGenerator.java
+++ projects/TeachingManagement/MybatisGenerator/src/main/java/MybatisGenerator.java
@@ -1,7 +1,18 @@
 import org.apache.ibatis.io.Resources;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Mybatis数据库逆向
@@ -10,10 +21,25 @@ import java.io.InputStream;
  */
 public class MybatisGenerator {
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger("MybatisGenerator");
         try (InputStream configStream = Resources.getResourceAsStream("generator.xml")) {
+            List<String> warnings = new ArrayList<>();
+            ConfigurationParser parser = new ConfigurationParser(warnings);
+            Configuration config = parser.parseConfiguration(configStream);
+            DefaultShellCallback callback = new DefaultShellCallback(true);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
             System.out.println(configStream);
         } catch (IOException e) {
-            System.out.println("配置文件加载失败");
+            logger.error("配置文件加载失败");
+        } catch (XMLParserException e) {
+            logger.error("配置文件解析失败");
+        } catch (InterruptedException e) {
+            logger.error("数据库逆向异常中断");
+        } catch (InvalidConfigurationException e) {
+            logger.error("配置文件存在非法配置");
+        } catch (SQLException e) {
+            logger.error("SQL语句执行失败");
         }
     }
 }
