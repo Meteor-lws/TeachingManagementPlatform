5b917829543af4c3577537e8b96908e1c26dad88
lws <chenkun_lws@126.com>
2017-04-08 09:16:27 +0000

    新建逆向模块

---
+++ projects/TeachingManagement/MybatisGenerator/src/main/java/MybatisGenerator.java
@@ -0,0 +1,10 @@
+/**
+ * Mybatis数据库逆向
+ *
+ * Created by lws on 2017/4/8.
+ */
+public class MybatisGenerator {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
