6b5e74a933243ae423acde18a357d14e7b98bc54
zg <651413411@qq.com>
2017-04-21 08:52:55 +0000

    上传缺失文件

---
+++ projects/TeachingManagement/ExaminationOnline/src/main/java/com/ruicai/examination/vo/question/Question.java
@@ -0,0 +1,53 @@
+package com.ruicai.examination.vo.question;
+
+import com.ruicai.examination.po.examination.ExaminationQuestion;
+
+/**
+ * Created by 朱高 on 2017/4/18.
+ */
+public class Question extends ExaminationQuestion {
+    private String questionSubjectName;
+    private String questionTypeName;
+    private String questionStageName;
+
+    public String getQuestionStageName() {
+        return questionStageName;
+    }
+
+    public void setQuestionStageName(String questionStageName) {
+        this.questionStageName = questionStageName;
+    }
+
+    public String getQuestionSubjectName() {
+        return questionSubjectName;
+    }
+
+    public void setQuestionSubjectName(String questionSubjectName) {
+        this.questionSubjectName = questionSubjectName;
+    }
+
+    public String getQuestionTypeName() {
+        return questionTypeName;
+    }
+
+    public void setQuestionTypeName(String questionTypeName) {
+        this.questionTypeName = questionTypeName;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id='" + id + '\'' +
+                ", questionSubject='" + questionSubject + '\'' +
+                ", questionSubjectName='" + questionSubjectName + '\'' +
+                ", questionTypeName='" + questionTypeName + '\'' +
+                ", questionType='" + questionType + '\'' +
+                ", questionStageName='" + questionStageName + '\'' +
+                ", questionScore=" + questionScore +
+                ", questionContent='" + questionContent + '\'' +
+                ", questionAnswer='" + questionAnswer + '\'' +
+                ", questionSelections=" + questionSelections +
+                ", questionStage='" + questionStage + '\'' +
+                '}';
+    }
+}
---
+++ projects/TeachingManagement/ExaminationOnline/src/main/java/com/ruicai/examination/vo/question/QuestionDatagrid.java
@@ -0,0 +1,27 @@
+package com.ruicai.examination.vo.question;
+
+import java.util.List;
+
+/**
+ * Created by 朱高 on 2017/4/18.
+ */
+public class QuestionDatagrid {
+    private long total;
+    private List<Question> rows;
+
+    public long getTotal() {
+        return this.total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<Question> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<Question> rows) {
+        this.rows = rows;
+    }
+}
